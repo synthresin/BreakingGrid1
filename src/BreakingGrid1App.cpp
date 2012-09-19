6a869d91e6aa31c0380dc54d1b6b530f285995bd
synthresin <synthresin@me.com>
2012-09-20 06:40:21 +0900

    좀더 수정

--- src/BreakingGrid1App.cpp
+++ src/BreakingGrid1App.cpp
@@ -1,34 +1,83 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Rand.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+#define GRID_NUM 20
+
 class BreakingGrid1App : public AppBasic {
   public:
+    void prepareSettings( Settings *settings );
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseDown( MouseEvent event );
+    void mouseMove( MouseEvent event );
 	void update();
 	void draw();
+    
+    float mGridSizeX;
+    float mGridSizeY;
+    
+    float mSize;
+    float mRandMax;
 };
+
+void BreakingGrid1App::prepareSettings( Settings *settings )
+{
+    settings->setWindowSize(700, 700);
+}
 
 void BreakingGrid1App::setup()
 {
+    mGridSizeX = (float)getWindowWidth() / GRID_NUM;
+    mGridSizeY = (float)getWindowHeight() / GRID_NUM;
+    
+    mSize = 5.0f;
+    
+    gl::enableAlphaBlending();
+    glLineWidth(5.0f);
 }
 
 void BreakingGrid1App::mouseDown( MouseEvent event )
 {
 }
 
+void BreakingGrid1App::mouseMove( MouseEvent event )
+{
+    
+    mSize = lmap((float)event.getX(), 0.0f, (float)getWindowWidth(), 0.0f, 30.0f);
+    
+    mRandMax = lmap((float)event.getY(), 0.0f, (float)getWindowHeight(), 0.0f, 10.0f);
+}
+
+
 void BreakingGrid1App::update()
 {
+    glLineWidth(mSize/2);
 }
 
 void BreakingGrid1App::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 1, 1, 1 ) );
+    gl::color(0, 0, 0, 0.5);
+    for (int yNum = 0; yNum < GRID_NUM; yNum++) {
+        for (int xNum = 0; xNum < GRID_NUM; xNum++) {
+            Vec2f center = Vec2f(xNum * mGridSizeX + mGridSizeX * 0.5 ,yNum * mGridSizeY + mGridSizeY * 0.5);
+            Vec2f randVec = Vec2f(Rand::randFloat(-mRandMax, mRandMax), Rand::randFloat(-mRandMax, mRandMax));
+            
+            center += randVec;
+            
+            gl::pushMatrices();
+            gl::translate(center);
+            gl::drawStrokedCircle(Vec2f::zero(), mSize);
+            
+            gl::popMatrices();
+            
+        }
+    }
 }
 
 CINDER_APP_BASIC( BreakingGrid1App, RendererGl )
