36566cbe80c26b4015a2a0c09d85853060f719d4
synthresin <synthresin@me.com>
2012-09-20 12:01:49 +0900

    고정된 랜덤값 증감 시키기로 변경

--- src/BreakingGrid1App.cpp
+++ src/BreakingGrid1App.cpp
@@ -22,6 +22,12 @@ class BreakingGrid1App : public AppBasic {
     
     float mSize;
     float mRandMax;
+    
+    float mFixedRandTweak;
+    
+    float mFixedRandMax;
+    
+    Vec2f mRandVec[GRID_NUM][GRID_NUM];
 };
 
 void BreakingGrid1App::prepareSettings( Settings *settings )
@@ -36,8 +42,15 @@ void BreakingGrid1App::setup()
     
     mSize = 5.0f;
     
+    mFixedRandMax = 30.0f;
     gl::enableAlphaBlending();
     glLineWidth(mSize/2);
+    
+    for (int i = 0; i < GRID_NUM; i++) {
+        for (int j = 0; j < GRID_NUM; j++) {
+            mRandVec[i][j] = Vec2f(Rand::randFloat(-mFixedRandMax, mFixedRandMax), Rand::randFloat(-mFixedRandMax, mFixedRandMax));
+        }
+    }
 }
 
 void BreakingGrid1App::mouseDown( MouseEvent event )
@@ -50,6 +63,9 @@ void BreakingGrid1App::mouseMove( MouseEvent event )
     mSize = lmap((float)event.getX(), 0.0f, (float)getWindowWidth(), 0.0f, 30.0f);
     
     mRandMax = lmap((float)event.getY(), 0.0f, (float)getWindowHeight(), 0.0f, 10.0f);
+    
+    mFixedRandTweak = lmap((float)event.getY(), 0.0f, (float)getWindowHeight(), 0.0f, 1.0f);
+    
 }
 
 
@@ -66,9 +82,13 @@ void BreakingGrid1App::draw()
     for (int yNum = 0; yNum < GRID_NUM; yNum++) {
         for (int xNum = 0; xNum < GRID_NUM; xNum++) {
             Vec2f center = Vec2f(xNum * mGridSizeX + mGridSizeX * 0.5 ,yNum * mGridSizeY + mGridSizeY * 0.5);
-            Vec2f randVec = Vec2f(Rand::randFloat(-mRandMax, mRandMax), Rand::randFloat(-mRandMax, mRandMax));
             
-            center += randVec;
+//            Vec2f randVec = Vec2f(Rand::randFloat(-mRandMax, mRandMax), Rand::randFloat(-mRandMax, mRandMax));
+//            center += randVec;
+            
+            Vec2f currentRandVec = mRandVec[xNum][yNum] * mFixedRandTweak;
+            
+            center += currentRandVec;
             
             gl::pushMatrices();
             gl::translate(center);
